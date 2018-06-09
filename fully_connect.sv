bd9200e8c7eda37e22492324487dd7edd2431b34
wswslzp <wswslzp@outlook.com>
2018-06-09 23:07:29 +0800

    Add result_valid port to fully_connect.sv

--- fully_connect.sv
+++ fully_connect.sv
@@ -9,7 +9,8 @@ module fully_connect #
 	input wire [31:0] weight[feature_size-1:0][bias_size-1:0],
 	input wire [31:0] bias[bias_size-1:0],
 	input wire clk, rst_n, en,
-	output wire [31:0] result[batch_size-1:0][bias_size-1:0]
+	output reg [31:0] result[batch_size-1:0][bias_size-1:0]
+	output reg result_valid;
 );
 
 
@@ -17,11 +18,15 @@ localparam weight_rsize = feature_size;
 localparam weight_csize = bias_size;
 localparam result_size = bias_size;
 
-wire [31:0] res_tmp[batch_size-1:0][result_size-1:0];
+wire [31:0] mul_res[batch_size-1:0][result_size-1:0];
 wire [31:0] bias_en[batch_size-1:0][bias_size-1:0];
 wire [31:0] data_en[batch_size-1:0][feature_size-1:0];
 wire [31:0] weight_en[weight_rsize-1:0][weight_csize-1:0];
 //wire [31:0] bias_en[bias_size-1:0];
+reg [31:0] _mul_res[batch_size-1:0][result_size-1:0];
+reg _matmul_valid;
+//reg [31:0] _result[batch_size-1:0][bias_size-1:0];
+wire [31:0] result_tmp[batch_size-1:0][bias_size-1:0];
 
 //assign data_en = en ? data : {(batch_size*feature_size){32'b0}};
 //assign weight_en = en ? weight : {(weight_rsize*weight_csize){32'b0}};
@@ -53,19 +58,36 @@ matmul #(
 	.in2(weight_en),
 	.clk(clk),
 	.rst_n(rst_n),
-	.result(res_tmp)
+	.result(mul_res)
 );
+
+always @(posedge clk) begin
+	if (_mul_res != mul_res ) begin
+		_mul_res <= mul_res;
+		_matmul_valid <= 1;
+	end else begin
+		_matmul_valid <= 0;
+	end 
+end 
 
 matadd #(
 	.rsize(batch_size),
 	.csize(bias_size)
 ) ma (
-	.in1(res_tmp),
-	.in2(bias_en),
+	.in1(mul_res),
+	.in2(bias_en & _matmul_valid),
 	.clk(clk),
 	.rst_n(rst_n),
-	.result(result)
+	.result(result_tmp)
 );
 					
+always @(posedge clk) begin
+	if (result != result_tmp) begin
+		result <= result_tmp;
+		result_valid <= 1'b1;
+	end else begin
+		result_valid <= 1'b0;
+	end 
+end 
 
 endmodule
